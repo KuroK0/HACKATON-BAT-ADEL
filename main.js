d765da6f7ff067b1f97592b6bf0e87dd40fc91d4
KuroK0 <lomonoz15@gmail.com>
2022-11-07 13:00:01 +0600

    asdf

---
+++ main.js
@@ -0,0 +1,14 @@
+let setting = document.querySelector("#setting");
+let modal = document.querySelector(".modal");
+
+setting.addEventListener("click", function () {
+  //   let modal = bootstrap.Modal.getInstance(exampleModal);
+  // modal.style.display =  'block'
+  //   modal.hide();
+});
+
+let add = document.querySelector('#add');
+
+add.addEventListener("click", function(){
+    
+})
